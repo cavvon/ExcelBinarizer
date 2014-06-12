d1ae5d900d522702b79fcf29eac31599a4ea766a
ctsinc <cts@heartlandfunds.com>
2014-06-12 12:57:57 -0500

    Defaults and rerun options

--- XLSConverter/Program.cs
+++ XLSConverter/Program.cs
@@ -8,44 +8,84 @@ namespace XLSConverter
     {
         static void Main(string[] args)
         {
-            try
+            bool rerun = false;
+            do
             {
-                string inputDir, outputDir;
-
-                if (args.Length != 2)
+                rerun = false;
+                try
                 {
-                    Console.WriteLine("This program batch converts xls and xlsx files to xlsb.\n" +
-                        "Folder traversal is recursive.\nCmd Line Usage: Binarizer.exe <inputDirectory> <outputDirectory>\n");
-                    Console.WriteLine("Please enter input directory:");
-                    inputDir = Console.ReadLine().Replace("\"", "");
+                    string useDefault, inputDir, outputDir;
+                    bool rePrompt = true;
 
-                    Console.WriteLine("Output directory:");
-                    outputDir = Console.ReadLine().Replace("\"", "");
+                    if (args.Length != 2)
+                    {
+                        Console.WriteLine("This program batch converts xls and xlsx files to xlsb.\n" +
+                            "Folder traversal is recursive.\nCmd Line Usage: Binarizer.exe <inputDirectory> <outputDirectory>\n");
+
+                        do
+                        {
+                            Console.WriteLine("Use default directories (.\\input; .\\output) [y/n]?");
+                            useDefault = Console.ReadLine();
+                            if (useDefault.ToUpper().Equals("Y"))
+                            {
+                                rePrompt = false;
+                                inputDir = "input";
+                                outputDir = "output";
+                            }
+                            else if (useDefault.ToUpper().Equals("N"))
+                            {
+                                rePrompt = false;
+                                Console.WriteLine("Please enter input directory:");
+                                inputDir = Console.ReadLine().Replace("\"", "");
+
+                                Console.WriteLine("Output directory:");
+                                outputDir = Console.ReadLine().Replace("\"", "");
+                            }
+                            else
+                            {
+                                // Remove compiler warnings
+                                inputDir = null;
+                                outputDir = null;
+                            }
+
+                        } while (rePrompt);
+                    }
+                    else
+                    {
+                        inputDir = args[0];
+                        outputDir = args[1];
+                    }
+
+                    inputDir = PathOperations.VerifyRootedPath(inputDir);
+                    outputDir = PathOperations.VerifyRootedPath(outputDir);
+
+                    Console.WriteLine("Writing to " + outputDir);
+
+                    FileInfo[] toConvert = CollectFilesForConversion(inputDir);
+                    PrepareOutputDirectory(outputDir);
+
+                    using (ExcelInterop excel = new ExcelInterop())
+                    {
+                        excel.ConvertFiles(toConvert, inputDir, outputDir);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + " Exiting program...press enter to close");
+                    Console.ReadLine();
+                }
+
+                Console.WriteLine("Run again? [y/n]");
+                string rerunIn = Console.ReadLine();
+                if (rerunIn.ToUpper().Equals("Y"))
+                {
+                    rerun = true;
                 }
                 else
                 {
-                    inputDir = args[0];
-                    outputDir = args[1];
+                    rerun = false;
                 }
-
-                inputDir = PathOperations.VerifyRootedPath(inputDir);
-                outputDir = PathOperations.VerifyRootedPath(outputDir);
-
-                Console.WriteLine("Writing to " + outputDir);
-
-                FileInfo[] toConvert = CollectFilesForConversion(inputDir);
-                PrepareOutputDirectory(outputDir);
-
-                using (ExcelInterop excel = new ExcelInterop())
-                {
-                    excel.ConvertFiles(toConvert, inputDir, outputDir);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message + " Exiting program...press enter to close");
-                Console.ReadLine();
-            }
+            } while (rerun);
         }
 
         private static FileInfo[] CollectFilesForConversion(string inputDir)
